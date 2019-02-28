8c94df719334a630f0db98b60f3e0d71fdf89ad1
Ely Monroy <44978313+emonroy58@users.noreply.github.com>
2019-02-28 08:05:21 -0600

    Update movies.js

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,7 @@
 const objTitlePelis=[];
 //let i=0;
 const titlePelis ='Get out';
-const url = 'http://www.omdbapi.com/?t=';
+const url = 'https://www.omdbapi.com/?t=';
 
 
 const searchMovie = document.getElementById('tite-mov');
