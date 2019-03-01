7df93c98e30108801c43fcc66ba1ee6c14f3de2d
emonroy58 <emgmonroy@gmail.com>
2019-02-28 20:17:29 -0600

    Se realizo funcion por busqueda de coincidencia y pintado de data

--- src/movies.js
+++ src/movies.js
@@ -1,55 +1,54 @@
-const objTitlePelis=[];
+let  objTitlePelis=[];
 //let i=0;
-
-const url = 'https://www.omdbapi.com/?t=';
+ 
+const searchMovie = document.getElementById("src-title");
+const titleMov = document.getElementById("title-mov");
+function SearchData(){    
+    const titlePelis = searchMovie.value;
+    const url = `https://www.omdbapi.com/?apikey=4c2bc917&s=${titlePelis}`;
+    fetch(url) //lectura del archivo .json
+    .then(response => response.json())
+    .then(data => {
+     const dataSearch = data.Search;
+     objTitlePelis.push(dataSearch);  
+      viewMovies(objTitlePelis);
+    }) 
+  
+   .catch(err => (err)) 
+    
+}
+titleMov.addEventListener('click',SearchData);
 
 
 
 const productWrapper = document.getElementById('list-movies');
-const titleMov = document.getElementById('title-mov');
-const viewMovies = () => { //muestra pokemon por tipo   
-    lecturaData();
-    //const search = searchMovie.value
+const viewMovies = (objTitlePelis) => { //muestra peliculas de acuerdo a la busqueda por coincidencia   
+    let data=  objTitlePelis[0];
     let viewMovie = '';
-    for (let i in objTitlePelis) {
-      //console.log('Tipo Poekemon: ',pokemon);
-      viewMovie = viewMovie + `  
-        
-          <div class="card col-lg-3 col-md-6 col-sm-12">
-           <div class="card-body bg-dark" style="width = 20rem;">
-             <img src="${objTitlePelis[i].Poster}" class="card-img-top" alt="${objTitlePelis[i].Title}">
-             <div class="card-body">
-             <h5 id="product-name" class="card-title d-flex justify-content-center">${objTitlePelis[i].Title}</h5>
-             </div>
-           </div> 
-          </div>               
-          `
-          }
-              
-    productWrapper.innerHTML = viewMovie;
+
+    data.forEach(element => {
+        viewMovie += `          
+        <div class="card col-lg-3 col-md-6 col-sm-12">
+         <div class="card-body bg-dark" style="width = 20rem;">
+           <img src="${element.Poster}" class="card-img-top" alt="${element.Title}">
+           <div class="card-body">
+           <h5 id="product-name" class="card-title d-flex justify-content-center">${element.Title}</h5>
+           </div>
+         </div> 
+        </div>               
+        `
+    productWrapper.insertAdjacentHTML('beforeend', viewMovie);
+
+    });
+ 
     return viewMovie;
   }
-  titleMov.addEventListener('click', viewMovies); //fin muestreo de tarjetas
 
 
-  function lecturaData(){
-    const searchMovie = document.getElementById('src-title').value;
-    const titlePelis = searchMovie;
-     let url_pelis = url+titlePelis+'&apikey=4c2bc917'
-     fetch(url_pelis ) //lectura del archivo .json
-    .then(response => response.json())
-    .then(data => {
-      localStorage.setItem('data', JSON.stringify(data))
-     // console.log (data); 
-      objTitlePelis.push(data);
-      console.log(objTitlePelis);
-      return data;
-      
-    }) 
-    //console.log (data); 
   
-    .catch(err => (err))
-}
+
+
+
  
  
 
